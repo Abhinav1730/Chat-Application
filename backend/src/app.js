f99eb0245209cfb29197a58b3e03c329491e76c7
Abhinav1730 <abhinav.saxena1730@gmail.com>
2025-03-25 01:40:34 +0530

    minor fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-import userRoutes from "./routes/user.routes.js";
+import userRoutes from "./routes/user.route.js"
 import cookieParser from "cookie-parser";
 
 const app = express();
